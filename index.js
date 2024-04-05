04bd440717bb3ffbcefc5822ca271661eda3ec86
fpmeeloop <kootblad@live.nl>
2024-04-05 10:57:51 +0200

    comments voor TSJENZO

--- index.js
+++ index.js
@@ -9,21 +9,24 @@ import groupRoutes from './routes/groupRoutes.js'
 import { createServer } from 'http';
 import { Server as SocketIOServer } from 'socket.io';
 
+//Library om geheime variables uit ENV file te halen. Denk aan jwt string. databse connection string etc
 dotenv.config()
-
+//zet express library in app variable zodat we verzoeken via url kunnen sturen. 
 const app  = express()
 const port = process.env.PORT
-
+// beetje configuratie
 app.use(express.json())
 app.use(cors({origin:"http://localhost:5173" , credentials: true}))
 app.use(cookieParser())
-
+//url routes naar de controller functies. die kan de frontend dus gebruiken om json objecten op te vragen of shit te creeeren in onze db
+// Alle auth functies bijv in de controller functies staan in de auth routes. 
+// dus als de front end een fetch maakt naar 'api/auth/WELKEAUTHFUNCTIEDANOOK' krijgt ie terug wat er in die speficieke functie gereturnt wordt.
+// bij post request geef je een json object terug met de nieuw aangemaakt waarde. 
+// bij get geef je terug wat er geGET moet worden.
 app.use('/api/auth' , authRoutes)
 app.use('/api/groups', groupRoutes)
 
-
 app.use('/api/upload', uploadRoutes)
-
 
 mongoose.set("strictQuery" , true)
 
--- middleware/verifyUser.js
+++ middleware/verifyUser.js
@@ -6,17 +6,20 @@ dotenv.config()
 
 export const verifyUser = async (req, res, next) => {
 
+    // JWT is een env variable die jij aan moet maken (uiteindelijk wordt dat op de server het zelfde voor de hele app natuurlijk)
+    // Wanneer een gebruiker aangemaakt wordt word deze zelfde string gebruikt en in de token verwerkt. 
+    // Als deze functie de token uit de local storage van de gebruiker haalt wordt gegekeken of onze string gebruikt is bij het maken van de token
     const JWT_SECRET = process.env.JWT
     const { authorization } = req.headers;
-
+    // Token verification en uit localstorage haal functionaliteit zoals hier boven beschreven. 
     const token = authorization?.split("Bearer ")[1];
     if (!token) return res.status(403).json({ error: "Unauthorized request!" });
     const payload = verify(token, JWT_SECRET)
     const id = payload.id;
-  
+    // Zoek user naar hand van ID van opgehaalde token.
     const user = await User.findOne({ _id: id, tokens: token });
     if (!user) return res.status(403).json({ error: "Unauthorized request!" });
-
+    // geef de user details mee aan de volgende functie.
     req.user = {
       id: user._id,
       name: user.name,
